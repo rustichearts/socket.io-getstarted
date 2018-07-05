cd82dee5554eb05ac8af8d8ad7d6a75b331fcbb9
kamiyam <labs.k5m@gmail.com>
2018-07-05 19:55:43 +0900

    Integrating Socket.IO

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const app = require('express')();
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
 
 http.listen(3000, function(){
